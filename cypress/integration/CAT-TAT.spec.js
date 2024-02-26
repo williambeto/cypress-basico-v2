d6d2e471ca04f4e8c22828c92734ab9ac67d015a
José Willams <jwsilva@dgae.mec.pt>
2024-02-26 13:06:35 +0000

    Aula 2

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,8 +1,24 @@
 /// <reference types="Cypress" />
 
 describe("Central de Atendimento ao Cliente TAT", function () {
-  it("verifica o título da aplicação", function () {
+  this.beforeEach(function () {
     cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", function () {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it.only("preenche os campos obrigatórios e envia o formulário", function () {
+    const longText =
+      "Do mesmo modo, o surgimento do comércio virtual desafia a capacidade de equalização dos relacionamentos verticais entre as hierarquias.";
+    cy.get("#firstName").type("Beto");
+    cy.get("#lastName").type("Silva");
+    cy.get("#email").type("williambeto@hotmail.com");
+    cy.get("#open-text-area").type(longText, {
+      delay: 0,
+    });
+    cy.get("button[type='submit']").click();
+    cy.get(".success").should("be.visible");
   });
 });
